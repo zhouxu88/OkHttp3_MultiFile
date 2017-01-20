98487ecba662645f00d24d22dfb4324aa7190095
ITZhouXu <374952705@qq.com>
2017-01-20 14:59:22 +0800

    change

--- app/src/main/java/com/zx/okhttp3/MainActivity.java
+++ app/src/main/java/com/zx/okhttp3/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.zx.uploadlibrary.listener.ProgressListener;
 import com.zx.uploadlibrary.listener.impl.UIProgressListener;
+import com.zx.uploadlibrary.utils.OKHttpUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,10 +26,14 @@ public class MainActivity extends AppCompatActivity{
 
 
 
-    //提交文件到服务器的地址(使用的时候替换成自己的服务器地址)
+    //上传文件到服务器的地址(使用的时候替换成自己的服务器地址)
     private static final String POST_FILE_URL = "http://192.168.1.3:8080/UploadFileDemo/MutilUploadServlet";
+    //下载文件的地址
+    private static final String DOWNLOAD_TEST_URL = "http://oh0vbg8a6.bkt.clouddn.com/app-debug.apk";
 
-    private ProgressBar uploadProgress, downloadProgeress;
+    //下载的文件的存储路径
+    private static final String STORE_DOWNLOAD_FILE_PATH = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)  + File.separator +"download2.apk";
+    private ProgressBar uploadProgress, downloadProgress;
     private TextView uploadTV,downloadTv;
 
 
@@ -43,7 +48,7 @@ public class MainActivity extends AppCompatActivity{
     //初始化View
     private void initView() {
         uploadProgress = (ProgressBar) findViewById(R.id.upload_progress);
-        downloadProgeress = (ProgressBar) findViewById(R.id.download_progress);
+        downloadProgress = (ProgressBar) findViewById(R.id.download_progress);
         uploadTV = (TextView) findViewById(R.id.tv_upload_progress);
         downloadTv = (TextView) findViewById(R.id.tv_download_progress);
         findViewById(R.id.upload).setOnClickListener(new View.OnClickListener() {
@@ -55,7 +60,7 @@ public class MainActivity extends AppCompatActivity{
         findViewById(R.id.download).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //download();
+                download();
             }
         });
     }
@@ -94,7 +99,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onUIStart(long bytesWrite, long contentLength, boolean done) {
                 super.onUIStart(bytesWrite, contentLength, done);
-                Toast.makeText(getApplicationContext(),"开始下载",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"开始上传",Toast.LENGTH_SHORT).show();
             }
 
             //上传结束
@@ -102,13 +107,13 @@ public class MainActivity extends AppCompatActivity{
             public void onUIFinish(long bytesWrite, long contentLength, boolean done) {
                 super.onUIFinish(bytesWrite, contentLength, done);
                 //uploadProgress.setVisibility(View.GONE); //设置进度条不可见
-                Toast.makeText(getApplicationContext(),"下载结束",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"上传成功",Toast.LENGTH_SHORT).show();
 
             }
         };
 
 
-        //开始Post请求
+        //开始Post请求,上传文件
         OKHttpUtils.doPostRequest(POST_FILE_URL, initUploadFile(), uiProgressRequestListener, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -120,6 +125,61 @@ public class MainActivity extends AppCompatActivity{
                 Log.i("TAG", "success---->"+response.body().string());
             }
         });
+
+    }
+
+
+    //文件下载
+    private void download() {
+        //这个是非ui线程回调，不可直接操作UI
+        final ProgressListener progressResponseListener = new ProgressListener() {
+            @Override
+            public void onProgress(long bytesRead, long contentLength, boolean done) {
+                Log.i("TAG", "bytesRead:" + bytesRead);
+                Log.i("TAG", "contentLength:" + contentLength);
+                Log.i("TAG", "done:" + done);
+                if (contentLength != -1) {
+                    //长度未知的情况下回返回-1
+                    Log.i("TAG", (100 * bytesRead) / contentLength + "% done");
+                }
+                Log.i("TAG", "================================");
+            }
+        };
+
+
+        //这个是ui线程回调，可直接操作UI
+        final UIProgressListener uiProgressResponseListener = new UIProgressListener() {
+            @Override
+            public void onUIProgress(long bytesRead, long contentLength, boolean done) {
+                Log.i("TAG", "bytesRead:" + bytesRead);
+                Log.i("TAG", "contentLength:" + contentLength);
+                Log.i("TAG", "done:" + done);
+                if (contentLength != -1) {
+                    //长度未知的情况下回返回-1
+                    Log.i("TAG", (100 * bytesRead) / contentLength + "% done");
+                }
+                Log.i("TAG", "================================");
+                //ui层回调,设置下载进度
+                int progress = (int) ((100 * bytesRead) / contentLength);
+                downloadProgress.setProgress(progress);
+                downloadTv.setText("下载进度：" + progress +"%");
+            }
+
+            @Override
+            public void onUIStart(long bytesRead, long contentLength, boolean done) {
+                super.onUIStart(bytesRead, contentLength, done);
+                Toast.makeText(getApplicationContext(),"开始下载",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onUIFinish(long bytesRead, long contentLength, boolean done) {
+                super.onUIFinish(bytesRead, contentLength, done);
+                Toast.makeText(getApplicationContext(),"下载完成",Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        //开启文件下载
+        OKHttpUtils.downloadandSaveFile(DOWNLOAD_TEST_URL,STORE_DOWNLOAD_FILE_PATH,uiProgressResponseListener);
 
     }
 
